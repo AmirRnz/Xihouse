96851e1102c15f5f4e749503ea4eb7b75a34fbe3
AmirRnz <155143890+AmirRnz@users.noreply.github.com>
2025-03-07 23:54:06 +0330

    more debug

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -1,8 +1,8 @@
 import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
-import { createOrUpdateUser } from "@/lib/actions/user";
-import { UserDataType } from "@/types/types";
+// import { createOrUpdateUser } from "@/lib/actions/user";
+// import { UserDataType } from "@/types/types";
 
 export async function POST(req: Request) {
   const SIGNING_SECRET = process.env.SIGNING_SECRET;
